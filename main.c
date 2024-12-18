ab0bc2708fdca82e9271033b7c034a816b0a92c3
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-12-18 09:42:16 +0200

    parse_mac

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef uint8_t mac_t[6];
+
+void parse_mac(const char* str, mac_t mac)
+{
+    char* s = strdup(str);
+    char* octet = strtok(s, ":");
+    int i = 0;
+    
+    while (octet != NULL && i < 6)
+    {
+        mac[i++] = strtol(octet, NULL, 16);
+
+        octet = strtok(NULL, ":");
+    }
+
+    free(s);
+}
 
 int main()
 {
-    printf("Test\n");
+    mac_t t;
+    parse_mac("11:22:33:44:55:66", t);
+    printf("%x\n", t[5]);
 
     return 0;
 }
